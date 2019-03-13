662870431659475143b2dbdf0abf0006ef4eebfd
Stanislav Muhametsin <346799+stazz@users.noreply.github.com>
2019-03-13 23:01:22 +0200

    Fixing issue with emitting wrong .(dll|exe).config file.

---
+++ RedistListGenerator/Program.cs
@@ -0,0 +1,145 @@
+/*
+ * Copyright 2019 Stanislav Muhametsin. All rights Reserved.
+ *
+ * Licensed  under the  Apache License,  Version 2.0  (the "License");
+ * you may not use  this file  except in  compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed  under the  License is distributed on an "AS IS" BASIS,
+ * WITHOUT  WARRANTIES OR CONDITIONS  OF ANY KIND, either  express  or
+ * implied.
+ *
+ * See the License for the specific language governing permissions and
+ * limitations under the License. 
+ */
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection.Metadata;
+using System.Reflection.PortableExecutable;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace RedistListGenerator
+{
+   static class Program
+   {
+      // First arg - RedisList files base dir
+      // Second arg - The target frameworks that don't have their FrameworkList.xml generated
+      // Third arg - all possible .NET Desktop frameworks
+      // Fourth arg - values of FrameworkPathOverride-properties for each framework, in same order as third arg
+      public static void Main( String[] args )
+      {
+         var tfList = args[1];
+         var targetFrameworks = tfList.Split( ';', StringSplitOptions.RemoveEmptyEntries );
+         if ( targetFrameworks.Length > 0 )
+         {
+            var fpoList = args[3].Split( ';', StringSplitOptions.RemoveEmptyEntries );
+            var fpos = args[2].Split( ';', StringSplitOptions.RemoveEmptyEntries )
+               .Select( ( tfw, idx ) => (tfw, idx) )
+               .ToDictionary( tfw => tfw.tfw, tfw => fpoList[tfw.idx] );
+            var baseDir = args[0];
+
+            foreach ( var tf in targetFrameworks )
+            {
+               var fpo = fpos[tf];
+               if ( !Path.IsPathRooted( fpo ) )
+               {
+                  throw new Exception( $"Unrooted FPO: {fpo}." );
+               }
+               var xDoc = new XDocument( new XDeclaration( "1.0", "utf-8", null ), GenerateFileListElement( Path.GetFullPath( fpo ), tf ) );
+               var targetPath = Path.Combine( baseDir, tf, "FrameworkList.xml" );
+               var targetDir = Path.GetDirectoryName( targetPath );
+               if ( !Directory.Exists( targetDir ) )
+               {
+                  Directory.CreateDirectory( targetDir );
+               }
+
+               xDoc.Save( targetPath );
+            }
+
+         }
+      }
+
+      private static XElement GenerateFileListElement(
+         String assembliesDirectory,
+         String targetFramework
+         )
+      {
+         return new XElement( "FileList",
+            Directory.EnumerateFiles( assembliesDirectory, "*.dll", SearchOption.TopDirectoryOnly )
+               .OrderBy( assemblyPath => Path.GetFileNameWithoutExtension( assemblyPath ) )
+               .Select( assemblyPath => (Object) GenerateFileElement( assemblyPath ) )
+               .Where( o => o != null )
+               .Prepend( new XAttribute( "Redist", "Microsoft-Windows-CLRCoreComp." + String.Join( '.', targetFramework.SkipWhile( c => !Char.IsDigit( c ) ).Select( c => c.ToString() ) ) ) ) // e.g. net46 -> 4.6
+            );
+      }
+
+      private static XElement GenerateFileElement(
+         String assemblyPath
+         )
+      {
+         XElement retVal = null;
+         using ( var stream = File.Open( assemblyPath, FileMode.Open, FileAccess.Read, FileShare.Read ) )
+         {
+            using ( var reader = new PEReader( stream, PEStreamOptions.LeaveOpen ) )
+            {
+               MetadataReader mdReader = null;
+               try
+               {
+                  mdReader = reader.GetMetadataReader( MetadataReaderOptions.None );
+               }
+               catch ( InvalidOperationException )
+               {
+                  // Sometimes the DLL file is not managed, and that's ok
+               }
+
+               if ( mdReader != null && mdReader.IsAssembly )
+               {
+                  var assembly = mdReader.GetAssemblyDefinition();
+                  retVal = new XElement( "File",
+                     new XAttribute( "AssemblyName", mdReader.GetString( assembly.Name ) ),
+                     new XAttribute( "Version", assembly.Version ),
+                     new XAttribute( "PublicKeyToken", String.Join( "", mdReader.GetBlobBytes( assembly.PublicKey ).PublicKeyTokenFromPublicKey().Select( b => String.Format( "{0:x2}", b ) ) ) ),
+                     new XAttribute( "Culture", mdReader.GetString( assembly.Culture ).DefaultIfNullOrEmpty( "neutral" ) ),
+                     new XAttribute( "ProcessorArchitecture", "MSIL" ),
+                     new XAttribute( "InGac", "true" )
+                     );
+                  if ( assembly.Flags.HasFlag( System.Reflection.AssemblyFlags.Retargetable ) )
+                  {
+                     // This does not seem to be in actual use (yet) by ResolveAssemblyReferences task at least.
+                     retVal.Add( new XAttribute( "Retargetable", "true" ) );
+                  }
+
+                  // There is also some IsRedistRoot attribute but I am not sure of its full meaning
+               }
+            }
+         }
+
+         return retVal;
+      }
+
+      private static Byte[] PublicKeyTokenFromPublicKey( this Byte[] publicKey )
+      {
+         using ( var sha = SHA1.Create() )
+         {
+            // Public key token = reversed last 8 bytes of SHA1 of the full public key.
+            var token = sha.ComputeHash( publicKey ).TakeLast( 8 ).ToArray();
+            Array.Reverse( token );
+            return token;
+         }
+      }
+
+      private static String DefaultIfNullOrEmpty( this String str, String defaultValue )
+      {
+         return String.IsNullOrEmpty( str ) ? defaultValue : str;
+      }
+
+   }
+}
